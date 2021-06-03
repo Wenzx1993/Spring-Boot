af5ff7cc713bacf66ef8a0ea167252463f2a785d
Wenzx <53310640+Wenzx1993@users.noreply.github.com>
2021-06-03 23:06:09 +0800

    1.spring应用案例 2.中文乱码配置 3.spring-boot-devtools应用和原理解析 4.热部署排除资源

--- SpringBootDemo_01/src/main/java/com/example/springbootdemo/controller/TestController.java
+++ SpringBootDemo_01/src/main/java/com/example/springbootdemo/controller/TestController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {   
+@RequestMapping(produces = "application/json;charset=utf-8")
+public class TestController {
 
 
     @RequestMapping("test1")
